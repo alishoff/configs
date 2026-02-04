e5594312d455c3c83e98b2287bb8c13835535606
Orkhan Alishov <or.alishov@gmail.com>
2026-02-04 21:55:20 +0400

    Update configs

--- sync.sh
+++ sync.sh
@@ -10,6 +10,9 @@ cp ~/.gitignore_global git/
 cp ~/.config/Code/User/settings.json vscode/
 code --list-extensions > vscode/extensions.txt
 
+# Nano
+cp ~/.nanorc nano/
+
 # Push
 git add .
 git commit -m "Update configs"
